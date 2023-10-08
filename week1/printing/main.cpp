d919a126f1494087fe5b8726498ec85dbd44e3f5
Zumh <17211423+Zumh@users.noreply.github.com>
2023-10-08 14:20:00 -0400

    update the readme

---
+++ week1/printing/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// single line commend
+	/* multiple line comment */
+	cout << "Hello world!" << endl;
+	return (0);
+}
---
+++ week1/variables/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	// declaring (giving the variable a type)
+	int number;
+	bool true_or_false;
+	char letter;
+
+	// assigning (giving the variable a value)
+	number = 99;
+	true_or_false = true;
+	letter = 'a';
+
+	// accessing (retreving the value of the data by printing)
+	cout << number << endl;
+	cout << true_or_false << endl;
+	cout << letter << endl;
+
+	return 0;
+}
